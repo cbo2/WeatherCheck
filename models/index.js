4bfa536eb8a6d95912fdae2202960afcf5d82092
Craig Bomba <talk2cbo@gmail.com>
2018-09-13 09:04:02 -0500

    modified the password for dev/test environments to be driven by an environment variable DB_PASSWORD

--- models/index.js
+++ models/index.js
@@ -7,6 +7,7 @@ var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
+var dbPassword = process.env.DB_PASSWORD || "password";
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
@@ -14,7 +15,7 @@ if (config.use_env_variable) {
   var sequelize = new Sequelize(
     config.database,
     config.username,
-    config.password,
+    dbPassword,
     config
   );
 }
