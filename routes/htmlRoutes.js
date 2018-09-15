fd8124a7fd9785883bc515780e45294b7cba1e99
Rion <rionirvin>
2018-09-15 12:15:22 -0500

    Home page outline done

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,28 +1,28 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Weather Check✔️",
-        title: "Sign up for daily text messages for the weather of the day",
-        examples: dbExamples
-      });
+  app.get("/", function (req, res) {
+    // db.weather.findAll({}).then(function(dbExamples) {
+    res.render("index", {
+      msg: "Weather Check✔️",
+      title: "Sign up for daily text messages for the weather of the day",
     });
+    // });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+  app.get("/login", function (req, res) {
+    // db.weather.user({ where: { id: req.params.id } }).then(function (user) {
+    res.render("login", {
+      msg: "Welcome Back",
+      title: "Please Log In"
     });
+    // });
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
