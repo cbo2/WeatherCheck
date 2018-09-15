b44f997b0462dff347728565b1b52c7874a6b917
ulas <udegertekin@gmail.com>
2018-09-15 00:59:35 -0500

    database schemas are updated

--- models/userprofile.js
+++ models/userprofile.js
@@ -1,7 +1,78 @@
 module.exports = function(sequelize, DataTypes) {
   var UserProfile = sequelize.define("UserProfile", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+    username:{
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: { 
+      type:DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    zipcode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    timesunday: {
+      type: DataTypes.STRING,
+      // DATEONLY
+
+    },
+    timesunday: {
+      type: DataTypes.STRING,
+      // DATEONLY
+    },
+    timesunday: {
+      type: DataTypes.STRING,
+      // DATEONLY
+    },
+    timesunday: {
+      type: DataTypes.STRING,
+      // DATEONLY
+    },
+    timemonday: {
+      type: DataTypes.STRING,
+      // DATEONLY
+    },
+    timetuesday: {
+      type: DataTypes.STRING,
+      // DATEONLY
+    },
+    timewednesday: {
+      type: DataTypes.STRING,
+      // DATEONLY
+    },
+    timethursday: {
+      type: DataTypes.STRING,
+      // DATEONLY
+    },
+    timefriday: {
+      type: DataTypes.STRING,
+      // DATEONLY
+    },
+    timesaturday: {
+      type: DataTypes.STRING,
+      // DATEONLY
+    },
   });
   return UserProfile;
 };
+
+/**
+ * 
+ * var timepreferences = {
+ *  SUNDAY: 2:00pm,
+ *  MONDAY: 3:00pm,
+ *  TUESDAY: 3:00pm,
+ *  WEDNESDAY: 4:00pm, 
+ * }
+ * 
+ * 
+ */
--- models/weatherdata.js
+++ models/weatherdata.js
@@ -1,7 +1,27 @@
 module.exports = function(sequelize, DataTypes) {
   var WeatherData = sequelize.define("WeatherData", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+    forecast: {
+      text: DataTypes.STRING,
+    },  
+    zipcode: {
+      type: DataTypes.STRING,
+      
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+    },
+    hightemp: {
+      type: DataTypes.STRING,
+    },
+    lowtemp: {
+      type: DataTypes.STRING,
+    },
+    precipitation: {
+      type: DataTypes.STRING,
+    },
+    wind: {
+      type: DataTypes.STRING,
+    } 
   });
   return WeatherData;
 };
