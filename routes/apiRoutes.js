2d1c91724a78fbf14eb425855a64fa25e98666b3
Othoniel Virgen <ov7664@gmail.com>
2018-09-15 12:44:24 -0500

    prototyping the various functions for displaying the information

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -23,7 +23,8 @@ darksky
     .exclude('minutely,currently,flags')      // optional: exclude, string || array, refer to API documentation.
     .extendHourly(true)             // optional: extend, boolean, refer to API documentation.
     .get()                          // execute your get request.
-    .then((response) => {console.log(JSON.stringify(response));})
+    .then((response) => {console.log(JSON.stringify(response))
+    console.log("============> " + response.daily.data[0].humidity, response.daily.data[0].precipProbability)})
     .catch(console.log);
 
 
@@ -76,6 +77,17 @@ module.exports = function (app) {
     });
   });
 
+//   app.on('listening', function () {
+//     var dailyTask = schedule.scheduleJob('0 0 * * *', function () {
+//       console.log("======================= DAILY TASK RUNNER running at: " + moment().format() + " ======================");
+//       db.UserProfile.findAll({}).then((users) => {
+//         // console.log(JSON.stringify(users));
+//         users.map((user) => { console.log(user.username); });
+//       })
+//       .catch(console.log);
+//     });
+// });  
+
   function checkWeatherInterval() {
     console.log("Checking the weather now....." + moment().format());
   }
@@ -101,3 +113,36 @@ module.exports = function (app) {
     // });
   });
 };
+
+//For current day, get high, low, precipitation and hourly temp, humidity, and precipitation
+function currentDay(){
+  darksky
+  .latitude('41.8703')            // required: latitude, string || float.
+  .longitude('-87.6236')            // required: longitude, string || float.
+  .time(moment().format("MMM Do YY"))             // optional: date, string 'YYYY-MM-DD'.
+  .units('us')                    // optional: units, string, refer to API documentation.
+  .language('en')                 // optional: language, string, refer to API documentation.
+  .exclude('minutely,flags')      // optional: exclude, string || array, refer to API documentation.
+  .get()                          // execute your get request.
+  .then((response) => {
+    console.log(
+      "\n" + '---------------------------' +
+      "\n" + "This is today's forecast!" +
+      "\n" + "Today's high " + response.daily.data[0].temperatureHigh + 
+      "\n" + "Today's low" + response.daily.data[0].temperatureLow +
+      "\n" + "Chance of precipitation " + response.daily.data[0].precipProbability +
+      "\n" + "Hourly temp " + response.hourly.data[].temperature + //Have a number loop inside the array from 0-23 to hit the hours and display the hourly temp automatically...maybe have Math.Floor the moment and output that number and push it into the array at the current time
+      "\n" + "Hourly sensation " + response.hourly.data[0].apparentTemperature +
+      "\n" + "Hourly humidity " + response.hourly.data[0].humidity +
+      "\n" + "Hourly wind " + response.hourly.data[0].windSpeed + 
+      "\n" + "Hourly  " + response.hourly.data[0].precipProbability
+      
+    );
+  })
+  .catch(console.log);  
+}
+// console.log(currentDay());
+
+
+moment().format("H");
+console.log("This is the current hour: " + moment().format("H"));
