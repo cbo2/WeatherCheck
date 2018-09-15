94265efd19b31d837c49023e77b92b41227b9123
ulas <udegertekin@gmail.com>
2018-09-15 12:04:06 -0500

    synax error fixed

--- models/userprofile.js
+++ models/userprofile.js
@@ -1,78 +1,67 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var UserProfile = sequelize.define("UserProfile", {
-    username:{
-      type:DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
       allowNull: false
     },
     password: {
-      type: DataTypes.STRING,
-      allowNull: false,
+      type: DataTypes.STRING
+      allowNull: false
     },
-    name: { 
-      type:DataTypes.STRING,
-      allowNull: false,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
     phone: {
-      type: DataTypes.STRING,
-      allowNull: false,
+      type: DataTypes.STRING
+      allowNull: false
     },
     zipcode: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     timesunday: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING
       // DATEONLY
 
     },
     timesunday: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING
       // DATEONLY
     },
     timesunday: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING
       // DATEONLY
     },
     timesunday: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING
       // DATEONLY
     },
     timemonday: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING
       // DATEONLY
     },
     timetuesday: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING
       // DATEONLY
     },
     timewednesday: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING
       // DATEONLY
     },
     timethursday: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING
       // DATEONLY
     },
     timefriday: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING
       // DATEONLY
     },
     timesaturday: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING
       // DATEONLY
-    },
+    }
   });
   return UserProfile;
 };
 
-/**
- * 
- * var timepreferences = {
- *  SUNDAY: 2:00pm,
- *  MONDAY: 3:00pm,
- *  TUESDAY: 3:00pm,
- *  WEDNESDAY: 4:00pm, 
- * }
- * 
- * 
- */
--- models/weatherdata.js
+++ models/weatherdata.js
@@ -1,27 +1,23 @@
 module.exports = function(sequelize, DataTypes) {
   var WeatherData = sequelize.define("WeatherData", {
-    forecast: {
-      text: DataTypes.STRING,
-    },  
-    zipcode: {
-      type: DataTypes.STRING,
-      
-    },
     date: {
-      type: DataTypes.DATEONLY,
+      type: DataTypes.DATEONLY
     },
     hightemp: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING
     },
     lowtemp: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING
     },
     precipitation: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING
     },
     wind: {
-      type: DataTypes.STRING,
-    } 
+      type: DataTypes.STRING
+    },
+    zipcode: {
+      type: DataTypes.STRING
+    }
   });
   return WeatherData;
 };
