d116505c61c8629b189b8946d59c09136e79b2a8
Othoniel Virgen <ov7664@gmail.com>
2018-09-16 18:15:38 -0500

    cleaned up the apiroutes file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,38 +1,56 @@
 var db = require("../models");
-var YQL = require('yql');
-var yw = require('weather-yahoo');
 var moment = require('moment');
 moment().format();
-
 var schedule = require('node-schedule');
 
-//------------------------------------------
+// zipcode longitude/latitude converter stuff start-----------------------------------------------------------------------------------
 var NodeGeocoder = require('node-geocoder');
- 
 var options = {
-  provider: 'freegeoip',
+  provider: 'google',
  
   // Optional depending on the providers
   httpAdapter: 'https', // Default
-  apiKey: 'YOUR_API_KEY', // for Mapquest, OpenCage, Google Premier
+  apiKey: process.env.GOOGLE_GEOCODE_KEY, // for Mapquest, OpenCage, Google Premier
   formatter: null         // 'gpx', 'string', ...
 };
 var geocoder = NodeGeocoder(options);
+geocoder.geocode('60605', function(err, res) {
+  console.log("********************* GEOCODE Start **********************");
+  console.log("The latitude is: " + res[0].latitude);
+  console.log("The longitude is: " + res[0].longitude);
+  console.log("********************* GEOCODE End **********************");
+});
+// zipcode longitude/latitude converter stuff end--------------------------------------------------------------------------------
 
-// Or using Promise
-geocoder.geocode('60632')
-  .then(function(res) {
-    console.log(res);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
-//-----------------------------------------------------
- 
+// Twilio Stuff------------------------------------------------------------------------------------------------------------------
+var twilio = require('twilio');
+require("dotenv").config();
+
+var sid = process.env.TWILIO_SID;
+var token = process.env.TWILIO_TOKEN;
+
+console.log("the sid is: " + sid);
+console.log("the token is: " + token);
+
+// Find your account sid and auth token in your Twilio account Console.
+var client = new twilio(sid, token);
+
+// Send the text message.
+client.messages.create({
+  to: '+16309955170',
+  from: '+16307915544',
+  body: "started running the node app at: " + moment().format() + " !!"
+}).then((message) => console.log(message.sid))
+.catch(err => console.log(err));
+// end Twilio Stuff---------------------------------------------------------------------------------------------------------
+
+// Scheduler begins---------------------------------------------------------------------------------------------------------
 var j = schedule.scheduleJob('03 * * * *', function(){
   console.log('this runs only on the 3rd minute of each hour!!!');
 });
+// Scheduler end------------------------------------------------------------------------------------------------------------
 
+// Dark Sky API Start-------------------------------------------------------------------------------------------------------
 const DarkSky = require('dark-sky')
 // const darksky = new DarkSky(process.env.DARK_SKY)
 const darksky = new DarkSky("93d657f3bdf48bc91d9977b8e970f9dc")
@@ -61,23 +79,8 @@ darksky
             )
     })
     .catch(console.log);
+// Dark Sky API end-------------------------------------------------------------------------------------------------------------------
 
-
-    // app.use('/a-week-ago', async (req, res, next) => {
-    //   try {
-    //     const { latitude, longitude } = req.body
-    //     const forecast = await darksky
-    //       .options({
-    //         latitude,
-    //         longitude,
-    //         time: moment().subtract(1, 'weeks')
-    //       })
-    //       .get()
-    //     res.status(200).json(forecast)
-    //   } catch (err) {
-    //     next(err)
-    //   }
-    // });
 
 module.exports = function (app) {
   // Get all examples
@@ -87,6 +90,11 @@ module.exports = function (app) {
       res.json(dbExamples);
     });
   });
+
+  //Push Data into database
+  app.post("/api/weatherdata", function (req, res) {
+    db.WeatherData.create()
+  })
 
   // Create a new profile
   app.post("/api/profile", function (req, res) {
@@ -112,77 +120,20 @@ module.exports = function (app) {
     });
   });
 
-//   app.on('listening', function () {
-//     var dailyTask = schedule.scheduleJob('0 0 * * *', function () {
-//       console.log("======================= DAILY TASK RUNNER running at: " + moment().format() + " ======================");
-//       db.UserProfile.findAll({}).then((users) => {
-//         // console.log(JSON.stringify(users));
-//         users.map((user) => { console.log(user.username); });
-//       })
-//       .catch(console.log);
-//     });
-// });  
+  // Initialize the function on app start up
+  app.on('listening', function () {
+    var dailyTask = schedule.scheduleJob('0 0 * * *', function () {
+      console.log("======================= DAILY TASK RUNNER running at: " + moment().format() + " ======================");
+      db.UserProfile.findAll({}).then((users) => {
+        // console.log(JSON.stringify(users));
+        users.map((user) => { console.log(user.username); });
+      })
+      .catch(console.log);
+    });
+});  
 
   function checkWeatherInterval() {
     console.log("Checking the weather now....." + moment().format());
   }
   setInterval(checkWeatherInterval, 5000);
-
-  app.get("/api/getWeather", function (req, res) {
-    console.log("hit the get route /api/getWeather with body: " + req.body);
-
-    yw.getSimpleWeather('60605').then(function (response) {
-      console.log(response);
-      res.json(response);
-    });
-
-    // var query = new YQL('select * from weather.forecast where (location = 94089)');
-
-    // query.exec(function(err, data) {
-    //   if (err) { console.log("ERROR: " + err);}
-    //   console.log(JSON.stringify(data));
-    // var location = data.query.results.channel.location;
-    // var condition = data.query.results.channel.item.condition;
-
-    // console.log('The current weather in ' + location.city + ', ' + location.region + ' is ' + condition.temp + ' degrees.');
-    // });
-  });
-
-
-//For current day, get high, low, precipitation and hourly temp, humidity, and precipitation
-// function currentDay(){
-//   darksky
-//   .latitude('41.8703')            // required: latitude, string || float.
-//   .longitude('-87.6236')            // required: longitude, string || float.
-//   .time(moment().format("MMM Do YY"))             // optional: date, string 'YYYY-MM-DD'.
-//   .units('us')                    // optional: units, string, refer to API documentation.
-//   .language('en')                 // optional: language, string, refer to API documentation.
-//   .exclude('minutely,flags')      // optional: exclude, string || array, refer to API documentation.
-//   .get()                          // execute your get request.
-//   .then((response) => {
-//     console.log(
-//       "\n" + '---------------------------' +
-//       "\n" + "This is today's forecast!" +
-//       "\n" + "Today's high " + response.daily.data[0].temperatureHigh + 
-//       "\n" + "Today's low" + response.daily.data[0].temperatureLow +
-//       "\n" + "Chance of precipitation " + response.daily.data[0].precipProbability +
-//       "\n" + "Hourly temp " + response.hourly.data[moment().format("H")].temperature +
-//       "\n" + "Hourly sensation " + response.hourly.data[moment().format("H")].apparentTemperature +
-//       "\n" + "Hourly humidity " + response.hourly.data[moment().format("H")].humidity +
-//       "\n" + "Hourly wind speed " + response.hourly.data[moment().format("H")].windSpeed + 
-//       "\n" + "Hourly chance of rain " + response.hourly.data[moment().format("H")].precipProbability
-//     );
-//   })
-//   .catch(console.log);  
-// }
-// console.log(
-//   "\n" + "-----------------------------------------------" +
-//   currentDay() +
-//   "\n" + "-----------------------------------------------------------"
-// );
-
-
-moment().format("H");
-console.log("This is the current hour: " + moment().format("H"));
 };
-//----------------------------------------------------------------------------------------------------------------------
