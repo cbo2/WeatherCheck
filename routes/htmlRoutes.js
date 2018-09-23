5ba02669c2a9805962297c9200a2cdc346262954
Craig Bomba <talk2cbo@gmail.com>
2018-09-22 20:48:32 -0500

    cleaned up some code.  Added a modal to the profile page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,6 @@ var db = require("../models");
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
-    // db.weather.findAll({}).then(function(dbExamples) {
     res.render("index", {
       msg: "Weather Check✔️",
       title: "Sign up for daily out-the-door weather advise notifications",
@@ -13,7 +12,6 @@ module.exports = function (app) {
   });
 
   app.post("/signup", function (req, res) {
-    // db.weather.user({ where: { id: req.params.id } }).then(function (user) {
     res.render("create", {
       msg: "Welcome",
       title: "Please fill out information below to create a new account!"
@@ -22,7 +20,6 @@ module.exports = function (app) {
   });
 
   app.get("/signup", function (req, res) {
-    // db.weather.user({ where: { id: req.params.id } }).then(function (user) {
     res.render("create", {
       msg: "Welcome",
       title: "Please fill out information below to create a new account!"
@@ -44,7 +41,6 @@ module.exports = function (app) {
     console.log("Hitting the get route for /profile");
     res.render("profile", {
       msg: "Welcome Back",
-      // user: { "id": "2", "username": "cbo2", "password": "password", "name": "craig", "phoneNumber": "6309955170", "zipcode": "90210", "timePreference[Friday]": "06:30", "timePreference[Monday]": "10:53", "timePreference[Sunday]": "10:28", "timePreference[Tuesday]": "11:00", "timePreference[Saturday]": "09:30", "timePreference[Thursday]": "09:48", "timePreference[Wednesday]": "10:56", "createdAt": "2018-09-21T00:12:05.000Z", "updatedAt": "2018-09-21T00:12:05.000Z" }
     });
   });
 
